c06d6b001207a302d0f3453afd2a68ec4829255d
putrilaras856 <90946068+putrilaras856@users.noreply.github.com>
2024-01-05 20:12:01 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,313 @@
+import itertools
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import accuracy_score
+import streamlit as st
+import time
+import pickle
+
+# Load and preprocess the data
+with open("hungarian.data", encoding='Latin1') as file:
+    lines = [line.strip() for line in file]
+
+data = itertools.takewhile(
+    lambda x: len(x) == 76,
+    (' '.join(lines[i:(i + 10)]).split() for i in range(0, len(lines), 10))
+)
+
+df = pd.DataFrame.from_records(data)
+df = df.iloc[:, :-1]
+df = df.drop(df.columns[0], axis=1)
+df = df.astype(float)
+
+df.replace(-9.0, np.NaN, inplace=True)
+
+df_selected = df.iloc[:, [1, 2, 7, 8, 10, 14, 17, 30, 36, 38, 39, 42, 49, 56]]
+
+column_mapping = {
+    2: 'age',
+    3: 'sex',
+    8: 'cp',
+    9: 'trestbps',
+    11: 'chol',
+    15: 'fbs',
+    18: 'restecg',
+    31: 'thalach',
+    37: 'exang',
+    39: 'oldpeak',
+    40: 'slope',
+    43: 'ca',
+    50: 'thal',
+    57: 'target'
+}
+
+df_selected.rename(columns=column_mapping, inplace=True)
+
+numeric_columns = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'target']
+categorical_columns = [col for col in df_selected.columns if col not in numeric_columns]
+
+for column in df_selected.columns:
+    if column in numeric_columns:
+        df_selected[column].fillna(df_selected[column].median(), inplace=True)
+    else:
+        df_selected[column].fillna(df_selected[column].mode()[0], inplace=True)
+
+columns_to_drop = ['ca', 'slope', 'thal']
+df_selected = df_selected.drop(columns_to_drop, axis=1)
+df_clean = df_selected.copy()
+df_clean.drop_duplicates(inplace=True)
+
+X = df_clean.drop("target", axis=1)
+y = df_clean['target']
+
+# Scaling features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Applying SMOTE
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X_scaled, y)
+
+# Load the model
+model = pickle.load(open("grid_rf.pkl", 'rb'))
+
+# Predict and calculate accuracy
+y_pred = model.predict(X_resampled)
+accuracy = accuracy_score(y_resampled, y_pred)
+accuracy = round((accuracy * 100), 2)
+
+# Reverse scaling to get the original distribution of data
+X_original = scaler.inverse_transform(X_resampled)
+
+# Convert the numpy array back to a DataFrame, ensuring the column names match the original dataset
+feature_columns = X.columns  # Assuming X was a DataFrame before scaling
+df_final = pd.DataFrame(X_original, columns=feature_columns)
+
+# Add the 'target' column to the DataFrame
+df_final['target'] = y_resampled
+
+# ========================================================================================================================================================================================
+
+# STREAMLIT
+st.set_page_config(
+  page_title = "Hungarian Heart Disease",
+  page_icon = ":heart:"
+)
+
+st.title("Hungarian Heart Disease")
+st.write(f"**_Model's Accuracy_** :  :green[**{accuracy}**]% (:red[_Do not copy outright_])")
+st.write("")
+
+tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
+
+with tab1:
+  st.sidebar.header("**User Input** Sidebar")
+
+  age = st.sidebar.number_input(label=":violet[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
+  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
+  st.sidebar.write("")
+
+  sex_sb = st.sidebar.selectbox(label=":violet[**Sex**]", options=["Male", "Female"])
+  st.sidebar.write("")
+  st.sidebar.write("")
+  if sex_sb == "Male":
+    sex = 1
+  elif sex_sb == "Female":
+    sex = 0
+  # -- Value 0: Female
+  # -- Value 1: Male
+
+  cp_sb = st.sidebar.selectbox(label=":violet[**Chest pain type**]", options=["Typical angina", "Atypical angina", "Non-anginal pain", "Asymptomatic"])
+  st.sidebar.write("")
+  st.sidebar.write("")
+  if cp_sb == "Typical angina":
+    cp = 1
+  elif cp_sb == "Atypical angina":
+    cp = 2
+  elif cp_sb == "Non-anginal pain":
+    cp = 3
+  elif cp_sb == "Asymptomatic":
+    cp = 4
+  # -- Value 1: typical angina
+  # -- Value 2: atypical angina
+  # -- Value 3: non-anginal pain
+  # -- Value 4: asymptomatic
+
+  trestbps = st.sidebar.number_input(label=":violet[**Resting blood pressure** (in mm Hg on admission to the hospital)]", min_value=df_final['trestbps'].min(), max_value=df_final['trestbps'].max())
+  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['trestbps'].min()}**], :red[Max] value: :red[**{df_final['trestbps'].max()}**]")
+  st.sidebar.write("")
+
+  chol = st.sidebar.number_input(label=":violet[**Serum cholestoral** (in mg/dl)]", min_value=df_final['chol'].min(), max_value=df_final['chol'].max())
+  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['chol'].min()}**], :red[Max] value: :red[**{df_final['chol'].max()}**]")
+  st.sidebar.write("")
+
+  fbs_sb = st.sidebar.selectbox(label=":violet[**Fasting blood sugar > 120 mg/dl?**]", options=["False", "True"])
+  st.sidebar.write("")
+  st.sidebar.write("")
+  if fbs_sb == "False":
+    fbs = 0
+  elif fbs_sb == "True":
+    fbs = 1
+  # -- Value 0: false
+  # -- Value 1: true
+
+  restecg_sb = st.sidebar.selectbox(label=":violet[**Resting electrocardiographic results**]", options=["Normal", "Having ST-T wave abnormality", "Showing left ventricular hypertrophy"])
+  st.sidebar.write("")
+  st.sidebar.write("")
+  if restecg_sb == "Normal":
+    restecg = 0
+  elif restecg_sb == "Having ST-T wave abnormality":
+    restecg = 1
+  elif restecg_sb == "Showing left ventricular hypertrophy":
+    restecg = 2
+  # -- Value 0: normal
+  # -- Value 1: having ST-T wave abnormality (T wave inversions and/or ST  elevation or depression of > 0.05 mV)
+  # -- Value 2: showing probable or definite left ventricular hypertrophy by Estes' criteria
+
+  thalach = st.sidebar.number_input(label=":violet[**Maximum heart rate achieved**]", min_value=df_final['thalach'].min(), max_value=df_final['thalach'].max())
+  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['thalach'].min()}**], :red[Max] value: :red[**{df_final['thalach'].max()}**]")
+  st.sidebar.write("")
+
+  exang_sb = st.sidebar.selectbox(label=":violet[**Exercise induced angina?**]", options=["No", "Yes"])
+  st.sidebar.write("")
+  st.sidebar.write("")
+  if exang_sb == "No":
+    exang = 0
+  elif exang_sb == "Yes":
+    exang = 1
+  # -- Value 0: No
+  # -- Value 1: Yes
+
+  oldpeak = st.sidebar.number_input(label=":violet[**ST depression induced by exercise relative to rest**]", min_value=df_final['oldpeak'].min(), max_value=df_final['oldpeak'].max())
+  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['oldpeak'].min()}**], :red[Max] value: :red[**{df_final['oldpeak'].max()}**]")
+  st.sidebar.write("")
+
+  data = {
+    'Age': age,
+    'Sex': sex_sb,
+    'Chest pain type': cp_sb,
+    'RPB': f"{trestbps} mm Hg",
+    'Serum Cholestoral': f"{chol} mg/dl",
+    'FBS > 120 mg/dl?': fbs_sb,
+    'Resting ECG': restecg_sb,
+    'Maximum heart rate': thalach,
+    'Exercise induced angina?': exang_sb,
+    'ST depression': oldpeak,
+  }
+
+  preview_df = pd.DataFrame(data, index=['input'])
+
+  st.header("User Input as DataFrame")
+  st.write("")
+  st.dataframe(preview_df.iloc[:, :6])
+  st.write("")
+  st.dataframe(preview_df.iloc[:, 6:])
+  st.write("")
+
+  result = ":violet[-]"
+
+  predict_btn = st.button("**Predict**", type="primary")
+
+  st.write("")
+  if predict_btn:
+    # Collect inputs into a dataframe
+    input_df = pd.DataFrame([[
+        age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak
+    ]], columns=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak'])
+    
+    # Standardize the inputs
+    input_df_scaled = scaler.transform(input_df)
+    
+    # Make prediction
+    prediction = model.predict(input_df_scaled)[0]
+
+    bar = st.progress(0)
+    status_text = st.empty()
+
+    for i in range(1, 101):
+      status_text.text(f"{i}% complete")
+      bar.progress(i)
+      time.sleep(0.01)
+      if i == 100:
+        time.sleep(1)
+        status_text.empty()
+        bar.empty()
+
+    if prediction == 0:
+      result = ":green[**Healthy**]"
+    elif prediction == 1:
+      result = ":orange[**Heart disease level 1**]"
+    elif prediction == 2:
+      result = ":orange[**Heart disease level 2**]"
+    elif prediction == 3:
+      result = ":red[**Heart disease level 3**]"
+    elif prediction == 4:
+      result = ":red[**Heart disease level 4**]"
+
+  st.write("")
+  st.write("")
+  st.subheader("Prediction:")
+  st.subheader(result)
+
+with tab2:
+  st.header("Predict multiple data:")
+
+  sample_csv = df_final.iloc[:5, :-1].to_csv(index=False).encode('utf-8')
+
+  st.write("")
+  st.download_button("Download CSV Example", data=sample_csv, file_name='sample_heart_disease_parameters.csv', mime='text/csv')
+
+  st.write("")
+  st.write("")
+  file_uploaded = st.file_uploader("Upload a CSV file", type='csv')
+
+  if file_uploaded:
+    uploaded_df = pd.read_csv(file_uploaded)
+    # Standardize the uploaded data using the same scaler used for the training data
+    uploaded_df_scaled = scaler.transform(uploaded_df)
+
+    # Make predictions on the standardized data
+    prediction_arr = model.predict(uploaded_df_scaled)
+
+    bar = st.progress(0)
+    status_text = st.empty()
+
+    for i in range(1, 70):
+      status_text.text(f"{i}% complete")
+      bar.progress(i)
+      time.sleep(0.01)
+
+    result_arr = []
+
+    for prediction in prediction_arr:
+      if prediction == 0:
+        result = "Healthy"
+      elif prediction == 1:
+        result = "Heart disease level 1"
+      elif prediction == 2:
+        result = "Heart disease level 2"
+      elif prediction == 3:
+        result = "Heart disease level 3"
+      elif prediction == 4:
+        result = "Heart disease level 4"
+      result_arr.append(result)
+
+    uploaded_result = pd.DataFrame({'Prediction Result': result_arr})
+
+    for i in range(70, 101):
+      status_text.text(f"{i}% complete")
+      bar.progress(i)
+      time.sleep(0.01)
+      if i == 100:
+        time.sleep(1)
+        status_text.empty()
+        bar.empty()
+
+    col1, col2 = st.columns([1, 2])
+
+    with col1:
+      st.dataframe(uploaded_result)
+    with col2:
+      st.dataframe(uploaded_df)
